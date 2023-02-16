9ee40e71e4871af52cd40d68da1c2eb8af16102a
Erik <erik@turborilla.com>
2023-02-16 10:48:18 +0100

    Title screen

---
+++ Assets/Scripts/Menu/TitleScreen.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TitleScreen : MonoBehaviour
+{
+	public TMP_Text highscore;
+
+	public void Start() {
+		highscore.text = string.Format("High score: {0:D8}", PlayerPrefs.GetInt("highscore"));
+	}
+
+	public void OnBtnNewGame() {
+		// Hide menu
+		SceneManager.LoadScene("Main");
+
+		// Init game..
+	}
+
+	public void OnBtnQuit() {
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
